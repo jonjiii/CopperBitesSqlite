d5d09d813272f83008372e1782f76cbc203b0fd6
jose <jose.benitez@alumnos.ucn.cl>
2025-05-26 00:00:12 -0400

    feat: added the generate token function

---
+++ src/utils/generateToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id = '') => {
+    return new Promise((resolve, reject) => {
+        const payload = { id };
+
+        jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1h'},
+            (error, token) => {
+                if (error) {
+                    console.log(error);
+                    reject('Token generation failed');
+                } else {
+                    resolve(token);
+                }
+            });
+    });
+}
+
+module.exports = generateToken;
